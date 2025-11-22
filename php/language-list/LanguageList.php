d9454fc3b018ae0ce2be1bdbcbc7914c732f17a7
Jankiel <jankiel@local.com>
2025-11-22 22:59:12 +0100

    'Languages list' in php solved

---
+++ php/language-list/LanguageList.php
@@ -0,0 +1,32 @@
+<?php
+
+function language_list(...$languages)
+{
+    return $languages;
+}
+
+
+function add_to_language_list($languages, $new_language)
+{
+    $languages[] = $new_language;
+    return $languages;
+}
+
+function prune_language_list($languages)
+{
+    
+    $first = array_shift($languages);
+    unset($first);
+    return $languages;
+}
+
+
+function current_language($languages)
+{
+    return $languages[0];
+}
+
+function language_list_length($languages)
+{
+    return count($languages);
+}
