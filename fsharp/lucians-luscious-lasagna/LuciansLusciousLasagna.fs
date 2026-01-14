fdee8968066d36eee59621d1c4043a199b730dd9
Jankiel <jankiel@local.com>
2026-01-14 23:08:31 +0100

    'Hello World' in F# solved
    'Lucians Luscious Lasagna' in F# solved

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,15 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+
+
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven minutes = expectedMinutesInOven - minutes
+
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layers = 2 * layers
+// TODO: define the 'elapsedTimeInMinutes' function
+
+let elapsedTimeInMinutes layers minutes =
+    preparationTimeInMinutes layers + minutes
