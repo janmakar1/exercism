bb7d5d0188f362fde17cb91e6f4b1eab72469c30
Jankiel <jankiel@local.com>
2026-02-28 23:36:57 +0100

    'Strain' in php solved

---
+++ php/strain/Strain.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+class Strain
+{
+    public function keep(array $list, callable $predicate): array
+    {
+        $result = [];
+        foreach ($list as $elem) {
+            if ($predicate($elem) == true) {
+                $result[] = $elem;
+            }
+        }
+        return $result;
+    }
+
+    public function discard(array $list, callable $predicate): array
+    {
+        $result = [];
+        foreach ($list as $elem) {
+            if ($predicate($elem) != true) {
+                $result[] = $elem;
+            }
+        }
+        return $result;
+    }
+}
