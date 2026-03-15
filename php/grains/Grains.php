0e2c6be216bd14be0b7308ec0a30e7ece1664272
Jankiel <jankiel@local.com>
2026-03-15 22:42:01 +0100

    'Grains' in php corrected

--- php/grains/Grains.php
+++ php/grains/Grains.php
@@ -14,13 +14,13 @@ function square(int $number): string
 
 function total(): string
 {
-    $sum = 0.;
-    foreach(range(1, 64) as $number) {
-        $sum += (float) square($number);
-    }
+    // $sum = 0;
+    // foreach(range(1, 64) as $number) {
+    //     $sum += square($number);
+    // }
     // return (string) $sum; // returns scientific notation e+
     
-    return sprintf ("%u", $sum); // returns 0
+    // return sprintf ("%u", $sum); // returns 0
 
     /*returns number 1 greater that the correct value
     and with leading zeros */
@@ -29,4 +29,9 @@ function total(): string
     // return $parts[0];
 
     // return '18446744073709551615'; // xD
+
+    // return sprintf("%u", 2 ** 64 - 1); // that one also returns 0 
+    // - so probably issue is with the ints overflowing int range
+
+    return trim(shell_exec('echo "2 ^ 64 - 1" | bc'));
 }
