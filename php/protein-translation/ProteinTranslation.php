75847be957948832979880aead7efffcc9c54120
Jankiel <jankiel@local.com>
2026-03-20 23:05:57 +0100

    'Protein Translation' in php corrected
    'Triangle' in php corrected

--- php/protein-translation/ProteinTranslation.php
+++ php/protein-translation/ProteinTranslation.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 class ProteinTranslation
 {
-    const CODON_TO_PROTEIN = [
+    private const CODON_TO_PROTEIN = [
         "AUG" => "Methionine",
         "UUU" => "Phenylalanine",
         "UUC" => "Phenylalanine",
@@ -19,20 +19,20 @@ class ProteinTranslation
         "UGU" => "Cysteine",
         "UGC" => "Cysteine",
         "UGG" => "Tryptophan",
-        "UAA" => "STOP",
-        "UAG" => "STOP",
-        "UGA" => "STOP",
+        "UAA" => self::STOP,
+        "UAG" => self::STOP,
+        "UGA" => self::STOP,
     ];
-    const INVALID_CODON = -1;
+    private const INVALID_CODON = "not really a codon :D";
+    private const STOP = "STOP";
 
     public function getProteins(string $codons): array
     {
         $proteins = [];
-        $codonsArray = str_split($codons, 3);
 
-        foreach($codonsArray as $codon) {
+        foreach(str_split($codons, 3) as $codon) {
             $protein = static::CODON_TO_PROTEIN[$codon] ?? static::INVALID_CODON;
-            if ($protein === "STOP") {
+            if ($protein === static::STOP) {
                 break;
             } else if ($protein === static::INVALID_CODON) {
                 throw new InvalidArgumentException("Invalid codon");
--- php/triangle/Triangle.php
+++ php/triangle/Triangle.php
@@ -1,32 +1,11 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class Triangle
 {
-    public function __construct(private float $a, private float $b, private float $c) {
+    public function __construct(private float $a, private float $b, private float $c)
+    {
         if (!$this->checkIfTriangle()) {
             throw new Exception("Triangle conditions do not met");
         }
@@ -41,18 +20,18 @@ class Triangle
 
     private function checkIfTriangle(): bool
     {
-        return $this->a > 0 and
-            $this->b > 0 and
-            $this->c > 0 and
+        return $this->a > 0 &&
+            $this->b > 0 &&
+            $this->c > 0 &&
             $this->checkIfTriangleInequalityConditionSatisfied();
     }
 
     public function kind(): string
     {
-        if ($this->a === $this->b or
-            $this->a === $this->c or
+        if ($this->a === $this->b ||
+            $this->a === $this->c ||
             $this->b === $this->c) {
-            if ($this->a === $this->b and
+            if ($this->a === $this->b &&
             $this->a === $this->c) {
                 return "equilateral";
             } else {
@@ -63,11 +42,3 @@ class Triangle
         }
     }
 }
-
-// $tr = new Triangle(10, 20, 30);
-
-// var_dump($tr->a);
-// var_dump($tr->b);
-// var_dump($tr->c);
-
-// var_dump($tr);
