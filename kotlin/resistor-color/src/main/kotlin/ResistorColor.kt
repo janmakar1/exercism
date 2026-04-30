f6cbb2918283ccab3bc6367d777fddf17137e5eb
jankiel <janmakar1@users.noreply.github.com>
2026-04-30 07:08:37 +0200

    'Resistor Color' in Kotlin solved

---
+++ kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
@@ -0,0 +1,22 @@
+object ResistorColor {
+
+    fun colorCode(input: String): Int {
+       return colors().indexOf(input)
+    }
+
+    fun colors(): List<String> {
+        return listOf(
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white"
+	)
+    }
+
+}
