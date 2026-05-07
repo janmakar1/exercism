24f7f795c9492be7ac88e6309643901e34b7d0b1
jankiel <janmakar1@users.noreply.github.com>
2026-05-07 17:54:24 +0200

    'Leap' in Haskell corrected

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,15 +1,4 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year =
---  by4 = mod year 4
---  by100 = mod year 100
---  by400 = mod year 400
-  if mod year 400 == 0 then
-    True
-  else
-    if mod year 100 == 0 then
-      False
-    else
-      mod year 4 == 0
-
+isLeapYear year = mod year 400 == 0 || (mod year 100 /= 0 && mod year 4 == 0)
