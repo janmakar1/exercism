447a9d3efb5278c028103057ad38e156c77f07bd
jankiel <janmakar1@users.noreply.github.com>
2026-05-12 09:43:18 +0200

    'Gigasecond' in Rust solved

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,8 @@
+use time::PrimitiveDateTime as DateTime;
+use std::time::Duration;
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    let gigasecond = Duration::new(1_000_000_000, 0);
+    start + gigasecond
+}
