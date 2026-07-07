0dc633e023b68aa67f08eedc20b4dbfcdbc477bb
Jankiel <janmakar1@users.noreply.github.com>
2026-07-07 23:05:42 +0200

    'Reverse String' in Lua solved
    'Leap' in Lua corrected

--- lua/leap/leap.lua
+++ lua/leap/leap.lua
@@ -1,11 +1,4 @@
 local leap_year = function(number)
-  if number % 400 == 0 then
-    return true
-  elseif number % 100 == 0 then
-    return false
-  else
-    return number % 4 == 0
+  return number % 400 == 0 or (number % 100 ~= 0 and number % 4 == 0)
   end
-end
-
 return leap_year
