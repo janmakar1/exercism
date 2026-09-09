02f81dc497125faa95de82cce8785e28e21d33ff
Jankiel <janmakar1@users.noreply.github.com>
2026-09-09 20:49:21 +0200

    'Triangle' in Rust solved

---
+++ rust/triangle/src/lib.rs
@@ -0,0 +1,33 @@
+pub struct Triangle {
+    a: u64,
+    b: u64,
+    c: u64,
+}
+
+impl Triangle {
+    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
+        let a = sides[0];
+        let b = sides[1];
+        let c = sides[2];
+
+        if a == 0 || b == 0 || c == 0 {
+            None
+        } else if a + b >= c && b + c >= a && a + c >= b {
+            Some(Self{ a, b, c })
+        } else {
+            None
+        }
+    }
+
+    pub fn is_equilateral(&self) -> bool {
+        self.a == self.b && self.b == self.c
+    }
+
+    pub fn is_scalene(&self) -> bool {
+        self.a != self.b && self.a != self.c && self.b != self.c
+    }
+
+    pub fn is_isosceles(&self) -> bool {
+        self.a == self.b || self.a == self.c || self.b == self.c
+    }
+}
